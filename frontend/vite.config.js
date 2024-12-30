4742c7ec817489e3201fbee95705c3b5c9b83944
Aman-Pathak-0243 <amanpathakiitj@gmail.com>
2024-12-31 04:45:05 +0530

    updated vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -11,5 +7,8 @@ export default defineConfig({
         secure: false,
       },
     },
+  },
+  build: {
+    outDir: "dist", // Ensure this matches the desired output directory
   },
 });
