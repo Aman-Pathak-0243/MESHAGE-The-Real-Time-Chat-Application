33d3a9583f033c94fbf0f8b98dd14558ce784523
Aman-Pathak-0243 <amanpathakiitj@gmail.com>
2024-12-31 04:35:43 +0530

    updated dbConnect

--- backend/DB/dbConnect.js
+++ backend/DB/dbConnect.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
-const dbConnect = async()=>{
-    try {
-        await mongoose.connect(process.env.MONGODB_CONNECT),
-        console.log("DB connected Succesfully");
-    } catch (error) {
-        console.log(console.error);
-    }
-}
- 
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECT, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("DB connected successfully");
+  } catch (error) {
+    console.error("Error connecting to DB:", error); // Properly logging the error
+  }
+};
 
-export default dbConnect
+export default dbConnect;
