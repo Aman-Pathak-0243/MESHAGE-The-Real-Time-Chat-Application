754b17b2fb5fe6683c65ab3fb5ccebf9ab180ba1
Aman-Pathak-0243 <amanpathakiitj@gmail.com>
2024-12-31 04:28:01 +0530

    updating vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/api':{
-        target:'https://slrtech-chatapp.onrender.com/',
-        secure:false
-      }
-    }
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://meshage-the-real-time-chat-application.onrender.com/",
+        secure: false,
+      },
+    },
   },
-})
+});
